75f8af2059caeef0a8132217226107982292c377
Anna Iosif <anna@ushahidi.com>
2026-08-25 14:06:28 +0200

    Adding interface definitions for core objects #5

---
+++ src/models.ts
@@ -0,0 +1,21 @@
+export type ActivityCategory = 'food' | 'transport' | 'sightseeing';
+
+export interface Activity {
+	id: string;
+	name: string;
+	cost: number;
+	category: ActivityCategory;
+	startTime: Date;
+}
+
+export interface Trip {
+	id: string;
+	destination: CountryInfo;
+	startDate: Date;
+	activities: Activity[];
+}
+
+export interface CountryInfo {
+	currency: string;
+	flag: string;
+}
