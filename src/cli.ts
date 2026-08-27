fc073928987f91e3017422472aa37989dc08411d
Anna Iosif <anna@ushahidi.com>
2026-08-27 18:40:13 +0200

    Changes from running prettier

--- src/cli.ts
+++ src/cli.ts
@@ -1,48 +1,55 @@
-import { getDestinationInfo } from './services/destinationService.ts';
-import { addActivityToTrip, createTrip, getActivitiesByDate, getTrips } from './services/itineraryService.ts';
+import { getDestinationInfo } from "./services/destinationService.ts";
+import {
+  addActivityToTrip,
+  createTrip,
+  getActivitiesByDate,
+  getTrips,
+} from "./services/itineraryService.ts";
 
 const main = async () => {
-    const destinationInfo = await getDestinationInfo('Norway');
-    console.log('Destination info about Norway fetched successfully');
-    console.log(destinationInfo);
+  const destinationInfo = await getDestinationInfo("Norway");
+  console.log("Destination info about Norway fetched successfully");
+  console.log(destinationInfo);
 
-     const newTrip = await createTrip(destinationInfo, new Date('2026-10-01'));
-     console.log('New trip created successfully');
-     console.log(newTrip);
+  const newTrip = await createTrip(destinationInfo, new Date("2026-10-01"));
+  console.log("New trip created successfully");
+  console.log(newTrip);
 
-     const allTrips = await getTrips();
-     console.log('All trips retrieved successfully');
-     console.log(allTrips);
+  const allTrips = await getTrips();
+  console.log("All trips retrieved successfully");
+  console.log(allTrips);
 
-     const tripId = newTrip.id;
-     const activity1 = {
-         name: 'Visit the fjords',
-         description: 'A very nice and scenic boat tour of the fjords',
-         cost: 100,
-         category: 'sightseeing' as const,
-         startTime: new Date('2026-10-02T10:00:00')
-     };
+  const tripId = newTrip.id;
+  const activity1 = {
+    name: "Visit the fjords",
+    description: "A very nice and scenic boat tour of the fjords",
+    cost: 100,
+    category: "sightseeing" as const,
+    startTime: new Date("2026-10-02T10:00:00"),
+  };
 
-     let updatedTrip = await addActivityToTrip(tripId, activity1);
-     console.log('Activity added to trip successfully');
-     console.log(updatedTrip);
+  let updatedTrip = await addActivityToTrip(tripId, activity1);
+  console.log("Activity added to trip successfully");
+  console.log(updatedTrip);
 
-     const activity2 = {
-        name: 'Walk in the mountains',
-        description: "A tough walk along the mountain",
-        cost: 50,
-        category: "sightseeing" as const,
-        startTime: new Date("2026-10-05T10:00:00")
-    }
-    
-    updatedTrip = await addActivityToTrip(tripId, activity2);
-    console.log('Activity added to trip successfully');
-    console.log(updatedTrip);
+  const activity2 = {
+    name: "Walk in the mountains",
+    description: "A tough walk along the mountain",
+    cost: 50,
+    category: "sightseeing" as const,
+    startTime: new Date("2026-10-05T10:00:00"),
+  };
 
-    const activitiesOnDay = await getActivitiesByDate(tripId, new Date("2026-10-05T10:00:00"));
-    console.log('Activities found!');
-    console.log(activitiesOnDay);
-    
-}
+  updatedTrip = await addActivityToTrip(tripId, activity2);
+  console.log("Activity added to trip successfully");
+  console.log(updatedTrip);
+
+  const activitiesOnDay = await getActivitiesByDate(
+    tripId,
+    new Date("2026-10-05T10:00:00"),
+  );
+  console.log("Activities found!");
+  console.log(activitiesOnDay);
+};
 
 main();
--- src/services/dbService.ts
+++ src/services/dbService.ts
@@ -1,69 +1,73 @@
-import { readFile, writeFile } from 'fs/promises';
-import type {  CountryInfo, Trip } from "../models.ts";
+import { readFile, writeFile } from "fs/promises";
+import type { CountryInfo, Trip } from "../models.ts";
 
-const dbFileUrl = new URL('../db.json', import.meta.url);
+const dbFileUrl = new URL("../db.json", import.meta.url);
 
-export const saveTripToDatabase = async (destination: CountryInfo, startDate: Date): Promise<Trip> => {
-    try {
-        const raw = await readFile(dbFileUrl, 'utf8');
-        const data = JSON.parse(raw);
-        const trips: Trip[] = data.trips;
-        const newTrip: Trip = {
-            id: String(trips.length + 1),
-            destination,
-            startDate,
-            activities: []
-        };
+export const saveTripToDatabase = async (
+  destination: CountryInfo,
+  startDate: Date,
+): Promise<Trip> => {
+  try {
+    const raw = await readFile(dbFileUrl, "utf8");
+    const data = JSON.parse(raw);
+    const trips: Trip[] = data.trips;
+    const newTrip: Trip = {
+      id: String(trips.length + 1),
+      destination,
+      startDate,
+      activities: [],
+    };
 
-        trips.push(newTrip);
-        data.trips = trips;
-        await writeFile(dbFileUrl, JSON.stringify(data, null, 2), 'utf8');
-        return newTrip;
-
-    } catch (error: unknown) {
-        throw new Error('Could not save trip to database', { cause: error });
-    }
-}
+    trips.push(newTrip);
+    data.trips = trips;
+    await writeFile(dbFileUrl, JSON.stringify(data, null, 2), "utf8");
+    return newTrip;
+  } catch (error: unknown) {
+    throw new Error("Could not save trip to database", { cause: error });
+  }
+};
 
 export const getTripsFromDatabase = async (): Promise<Trip[]> => {
-    try {
-        const raw = await readFile(dbFileUrl, 'utf8');
-        const data = JSON.parse(raw);
-        return data.trips;
-    } catch (error: unknown) {
-        throw new Error('Could not retrieve trips from database', { cause: error });
+  try {
+    const raw = await readFile(dbFileUrl, "utf8");
+    const data = JSON.parse(raw);
+    return data.trips;
+  } catch (error: unknown) {
+    throw new Error("Could not retrieve trips from database", { cause: error });
+  }
+};
+
+export const getTripFromDatabase = async (
+  tripId: string,
+): Promise<Trip | undefined> => {
+  try {
+    const raw = await readFile(dbFileUrl, "utf8");
+    const data = JSON.parse(raw);
+    const trips: Trip[] = data.trips;
+    return trips.find((trip) => trip.id === tripId);
+  } catch (error: unknown) {
+    throw new Error("Could not retrieve trip from database", { cause: error });
+  }
+};
+
+export const updateTripInDatabase = async (
+  updatedTrip: Trip,
+): Promise<void> => {
+  try {
+    const raw = await readFile(dbFileUrl, "utf8");
+    const data = JSON.parse(raw);
+    const trips: Trip[] = data.trips;
+    const tripIndex = trips.findIndex((trip) => trip.id === updatedTrip.id);
+
+    if (tripIndex !== -1) {
+      trips[tripIndex] = updatedTrip;
+      data.trips = trips;
+
+      await writeFile(dbFileUrl, JSON.stringify(data, null, 2), "utf8");
+    } else {
+      throw new Error(`Trip with id ${updatedTrip.id} not found`);
     }
-}
-
-export const getTripFromDatabase = async (tripId: string): Promise<Trip | undefined> => {
-    try {
-        const raw = await readFile(dbFileUrl, 'utf8');
-        const data = JSON.parse(raw);
-        const trips: Trip[] = data.trips;
-        return trips.find(trip => trip.id === tripId);
-    } catch (error: unknown) {
-        throw new Error('Could not retrieve trip from database', { cause: error });
-    }
-}
-
-
-
-export const updateTripInDatabase = async (updatedTrip: Trip): Promise<void> => {
-    try {
-        const raw = await readFile(dbFileUrl, 'utf8');
-        const data = JSON.parse(raw);
-        const trips: Trip[] = data.trips;
-        const tripIndex = trips.findIndex(trip => trip.id === updatedTrip.id);
-        
-        if (tripIndex !== -1) {
-            trips[tripIndex] = updatedTrip;
-            data.trips = trips;
-
-            await writeFile(dbFileUrl, JSON.stringify(data, null, 2), 'utf8');
-        } else {
-            throw new Error(`Trip with id ${updatedTrip.id} not found`);
-        }
-    } catch (error: unknown) {
-        throw new Error('Could not update trip in database', { cause: error });
-    }
-}
+  } catch (error: unknown) {
+    throw new Error("Could not update trip in database", { cause: error });
+  }
+};
