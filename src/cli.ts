77b950c471ad4ef7c26cec0da1bc73337bcb7f09
Anna Iosif <anna@ushahidi.com>
2026-08-27 19:18:28 +0200

    Adding function to find high-cost activities #9

--- src/cli.ts
+++ src/cli.ts
@@ -6,7 +6,10 @@ import {
   getActivitiesByDate,
   getTrips,
 } from "./services/itineraryService.ts";
-import { getTotalCostForTrip } from "./services/budgetService.ts";
+import {
+  getHighCostActivities,
+  getTotalCostForTrip,
+} from "./services/budgetService.ts";
 
 const main = async () => {
   const destinationInfo = await getDestinationInfo("Norway");
@@ -68,6 +71,10 @@ const main = async () => {
   console.log(
     `The total cost for this trip is ${totalCost} ${updatedTrip.destination.currency}`,
   );
+
+  const highCostActivities = await getHighCostActivities(tripId, 90);
+  console.log("I found the following high-cost activities:");
+  console.log(highCostActivities);
 };
 
 main();
--- src/services/budgetService.ts
+++ src/services/budgetService.ts
@@ -1,3 +1,4 @@
+import type { Activity } from "../models.ts";
 import { getTripFromDatabase } from "./dbService.ts";
 
 export const getTotalCostForTrip = async (tripId: string): Promise<number> => {
@@ -16,3 +17,21 @@ export const getTotalCostForTrip = async (tripId: string): Promise<number> => {
     throw new Error("Could not calculate the cost", { cause: error });
   }
 };
+
+export const getHighCostActivities = async (
+  tripId: string,
+  threshold: number,
+): Promise<Activity[]> => {
+  try {
+    const trip = await getTripFromDatabase(tripId);
+
+    if (!trip) {
+      throw new Error(`Could not find a trip with id ${tripId}`);
+    }
+
+    return trip.activities.filter((activity) => activity.cost >= threshold);
+  } catch (error: unknown) {
+    throw new Error("Could not get high cost activities", { cause: error });
+  }
+};
+
